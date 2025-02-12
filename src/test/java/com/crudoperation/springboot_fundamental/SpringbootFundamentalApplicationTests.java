b251b07fe1041202ab8f7104ba33c598e8ae9a4d
holibhai <146805057+holibhai@users.noreply.github.com>
2025-02-13 01:23:27 +0530

    Initial commit

---
+++ src/test/java/com/crudoperation/springboot_fundamental/SpringbootFundamentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudoperation.springboot_fundamental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFundamentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
