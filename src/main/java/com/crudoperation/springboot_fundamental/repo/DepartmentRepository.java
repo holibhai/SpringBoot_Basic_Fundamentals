fccf78b3cc61cfd3a26c3b636bd5193935256ac1
holibhai <146805057+holibhai@users.noreply.github.com>
2025-02-17 18:54:21 +0530

    Department repository added

---
+++ src/main/java/com/crudoperation/springboot_fundamental/repo/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.crudoperation.springboot_fundamental.repo;
+
+import com.crudoperation.springboot_fundamental.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Integer> {
+}
